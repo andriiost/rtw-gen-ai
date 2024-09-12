0bbb21ebb0e1427bfcebdcd3fa41eca44cc53789
sviatoslav0koval <sviatoslav@wsib-innovation.ca>
2024-09-12 12:42:46 -0400

    Update main.py
    added constrains and lists

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ for name, text in zip(data["name"], data["text"]):
     You will receive a text containing information about multiple accommodations. Extract each accommodation from the text with the following details:
     - Accommodation Name: The specific tool or method being used to accommodate workers. Retrieve directly from the text; do not create or alter names;;
     - Description: A detailed explanation of the accommodation.Get this info from the text directly. Retrieve directly from the text; do not create or alter description;ter names;
-    - Injury Location Name: Choosing only from the list: {} indentify the part of the body that the accommodation aims to protect or assist;
-    - Industry Name: Choosing only from the list: {} indentify the industry in which the accommodation is used (e.g., Construction);
-    - Activity Name: Choosing only from the list: {} indentify the physical tasks associated with or accommodated by the accommodation.
+    - Injury Location Name: Choosing only from the list: {"Body systems", "Multiple body parts", "Cranial region, including skull", "Leg(s)", "Lower back (lumbar, sacral, coccygeal regions)", "Shoulder", "Ankle(s)", "Finger(s), fingernail(s)", "Arm(s)", "Wrist(s)", "Not Coded", "Foot (feet), except toe(s)", "Chest, including ribs, internal organs", "Pelvic region", "Upper extremities, unspecified, NEC", "Multiple trunk locations", "Multiple lower extremities locations", "Hand(s), except finger(s)", "Upper back (cervical, thoracic regions)", "Multiple back regions", "Abdomen", "Back, unspecified, NEC", "Head, unspecified, NEC", "Eye(s)", "Face", "Toe(s), toenail(s)", "Ear(s)", "Multiple head locations", "Lower extremities, unspecified, NEC", "Trunk, unspecified, NEC", "Other body parts including unclassified, NEC"} indentify the part of the body that the accommodation aims to protect or assist;
+    - Industry Name: Choosing only from the list: {Agriculture, Automotive, Chemical/Process, Construction, Education, Electrical, Food, Forestry, Health Care, Manufacturing, Mining, Municipal, Primary Metals, Pulp & Paper, Services, Transportation} indentify the industry in which the accommodation is used (e.g., Construction);
+    - Activity Name: Indentify the physical tasks associated with or accommodated by the accommodation.
 
     Format the extracted data as a JSON object, with an array if multiple are mentioned in the text. Use the following structure:
     {{
