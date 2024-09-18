7a115fe341b573ccd3981968006de38944ee7476
jesseh <jesse_huang@wsib-innovation.ca>
2024-09-18 10:32:21 -0400

    Added frontend skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+      <p> hello world </p>
+    </div>
+  );
+}
+
+export default App;
