c86a17150ab543523cc135dba2c349aff2be031a
quarriea <alex_quarrie@wsib-innovation.ca>
2024-10-07 13:57:22 -0400

    responsive view for single accommodation page

--- frontend/src/pages/individualAcc/Accommodation.jsx
+++ frontend/src/pages/individualAcc/Accommodation.jsx
@@ -72,7 +72,8 @@ export default function Accommodation() {
               buttonLink="/accommodations"
             />
           ) : (
-            <div className="mx-48 my-16">
+            <div className="lg:mx-48 md:mx-12 mx-6 my-16">
+              <div className="flex justify-between">
               <Link to="/accommodations">
                 <button className="flex flex-row px-3 py-1 rounded-3xl my-8 border-cyan-950 border">
                   <div className="my-auto">
@@ -92,21 +93,33 @@ export default function Accommodation() {
                       </g>
                     </svg>
                   </div>
-                  Back
+                  <p className="ms-1">Back</p>
                 </button>
               </Link>
+              <Link to="/accommodations" className="lg:hidden block">
+                <button className="flex flex-row px-3 py-1 rounded-3xl my-8 border-cyan-950 border">
+                <p className="me-1">Share</p>
+                  <div className="my-auto">
+                  <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9.85938 2.80737L9.14062 3.52612L12.2812 6.66675H5.5C3.57227 6.66675 2 8.23901 2 10.1667C2 12.0945 3.57227 13.6667 5.5 13.6667V12.6667C4.11328 12.6667 3 11.5535 3 10.1667C3 8.78003 4.11328 7.66675 5.5 7.66675H12.2812L9.14062 10.8074L9.85938 11.5261L13.8594 7.52612L14.2031 7.16675L13.8594 6.80737L9.85938 2.80737Z" fill="#003359"/>
+</svg>
 
-              <div className="flex w-full flex-row justify-center py-10">
-                <div className="w-4/5 pe-32  flex flex-col">
-                  <p className="text-4xl mb-1">{title}</p>
+                  </div>
+                </button>
+              </Link>
+              </div>
 
-                  <div className="flex text-lg flex-row">
+              <div className="flex w-full flex-col lg:flex-row text-center justify-center py-10">
+                <div className="lg:w-4/5 lg:pe-32 w-full  flex flex-col">
+                  <p className="text-4xl text-left mb-1">{title}</p>
+
+                  <div className="flex text-left flex-col text-lg lg:flex-row">
                     {!verified ? (
                       <p> Created on {date}</p>
                     ) : (
                       <p>Verified on {date} </p>
                     )}
-                    <p className="ps-4 flex flex-row text-sky-600">
+                    <p className="lg:ps-4 flex flex-row text-sky-600">
                       <div className="mx-1 my-auto">
                         <svg
                           width="12"
@@ -125,34 +138,61 @@ export default function Accommodation() {
                       Suggest Edit
                     </p>
                   </div>
-                  <p className="mt-8">{accommodation}</p>
+                  <div className="lg:hidden block">
+                  <Link
+                    to={link}
+                    className="w-full justify-center flex flex-row text-white px-10 py-3 rounded-3xl mt-6 bg-sky-600"
+                  >
+                    View original PDF
+                    <div className="my-auto mx-2">
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <g id="external-link-alt-solid">
+                          <path
+                            id="Vector"
+                            d="M13.5 3.75V5.25H17.6719L8.46094 14.4609L9.53906 15.5391L18.75 6.32812V10.5H20.25V3.75H13.5ZM3.75 6.75V20.25H17.25V10.5L15.75 12V18.75H5.25V8.25H12L13.5 6.75H3.75Z"
+                            fill="white"
+                          />
+                        </g>
+                      </svg>
+                    </div>
+                  </Link>
+                  </div>
+                  <p className="mt-8 text-left">{accommodation}</p>
                 </div>
 
-                <div className="w-1/5">
-                  <div className="border p-6 rounded-lg border-neutral-400">
+                <div className="lg:w-1/5 mt-8 w-full">
+                  <div className="lg:border border-y p-6 lg:rounded-lg border-neutral-400">
                     <p className="text-2xl font-bold">More Information</p>
-
-                    <div>
-                      Part of Body
+                    <div className="flex lg:flex-col">
+                    <div className="w-1/2 lg:mx-0 mx-2 lg:w-auto">
+                      Part of Body:
                       <p className="rounded-3xl border-green-800 flex justify-center border py-3 px-1 mt-2 mb-6 hover:bg-green-800/25 text-green-800">
                         {area}
                       </p>
                     </div>
 
-                    <div>
-                      Nature of Injury
+                    <div className="w-1/2 lg:mx-0 mx-2 lg:w-auto">
+                      Nature of Injury:
                       <p className="rounded-3xl flex justify-center border-plum border py-3 px-1 mt-2 mb-6 hover:bg-plum/25 text-plum">
                         {nature}
                       </p>
                     </div>
+                    </div>
 
                     <div>
-                      NAICS Industry
+                      NAICS Industry:
                       <p className="rounded-3xl flex justify-center border-midnight hover:bg-midnight/25 border py-3 px-1 mt-2 mb-6 text-midnight">
                         {industry}
                       </p>
                     </div>
                   </div>
+                  <div className="hidden lg:block">
                   <Link
                     to={link}
                     className="w-full justify-center flex flex-row text-white px-10 py-3 rounded-3xl mt-6 bg-sky-600"
@@ -199,6 +239,7 @@ export default function Accommodation() {
                       </svg>
                     </div>
                   </button>
+                  </div>
                 </div>
               </div>
             </div>
