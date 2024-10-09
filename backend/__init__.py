0340fd8a23d0fdc113671d247de3b138db38ddcd
will-mcintyre <will_mcintyre@wsib-innovation.ca>
2024-10-09 10:35:26 -0400

    Added .env file check

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,6 +5,31 @@ from dotenv import load_dotenv
 from .extensions import db, ma, migrate
 from .routes.accommodation_routes import accommodation_routes
 from .routes.document_routes import document_routes
+import os
+
+# Required environment variables
+REQUIRED_ENV_VARS = [
+    "AZURE_OPENAI_API_KEY",
+    "AZURE_OPENAI_ENDPOINT",
+    "OPENAI_API_VERSION",
+    "AZURE_OPENAI_DEPLOYMENT_NAME",
+    "AZURE_STORAGE_ACCOUNT",
+    "AZURE_BLOB_KEY",
+    "AZURE_BLOB_CONTAINER",
+    "OPENAI_API_VERSION",
+    "DEVELOPMENT_DATABASE_URL"
+]
+
+def check_env_vars(required_vars):
+    """
+    Check if all required environment variables are set.
+    """
+    missing_vars = [var for var in required_vars if not os.getenv(var)]
+    
+    if missing_vars:
+        error_message = f"Missing environment variables: {', '.join(missing_vars)}"
+        raise EnvironmentError(error_message)
+
 
 def create_app(config_mode):
     """
@@ -19,6 +44,9 @@ def create_app(config_mode):
     """
     # Load environment variables
     load_dotenv()
+
+    # Check environment variables before initializing the app
+    check_env_vars(REQUIRED_ENV_VARS)
 
     # Create Flask application
     app = Flask(__name__)
